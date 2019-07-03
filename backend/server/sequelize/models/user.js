e73e487071761256573b9cb33e0f6ba510ca5e3d
Rasmus Jepsen <r.jepsen@uqconnect.edu.au>
2019-07-03 10:43:57 +0800

    Fixed model

--- backend/server/sequelize/models/user.js
+++ backend/server/sequelize/models/user.js
@@ -3,7 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
     profile: DataTypes.STRING,
-    sub: {type: DataTypes.STRING, primaryKey: true}
+    sub: DataTypes.STRING
   }, {});
   User.associate = function(models) {
     // associations can be defined here
