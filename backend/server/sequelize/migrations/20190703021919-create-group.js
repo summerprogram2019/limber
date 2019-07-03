6a760246bbfd7451778c17df6103b8eac1f0ab9f
Rasmus Jepsen <r.jepsen@uqconnect.edu.au>
2019-07-03 10:45:23 +0800

    Fixed weird group meme migration

--- backend/server/sequelize/migrations/20190703021919-create-group.js
+++ backend/server/sequelize/migrations/20190703021919-create-group.js
@@ -8,9 +8,6 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      id: {
-        type: Sequelize.INTEGER
-      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
