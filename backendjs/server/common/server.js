c1f3afeb1468ff54df2fd4a7ea6ff9a4dfc6e81b
Billy Schulze <billy@billyschulze.com>
2019-07-04 13:46:00 +1000

    typescript yeeted

---
+++ backendjs/server/common/server.js
@@ -0,0 +1,52 @@
+import Express from 'express';
+import * as path from 'path';
+import * as bodyParser from 'body-parser';
+import * as http from 'http';
+import * as os from 'os';
+import cookieParser from 'cookie-parser';
+
+import { OpenApiValidator } from 'express-openapi-validator';
+import errorHandler from '../api/middlewares/error.handler';
+
+import l from './logger';
+
+const app = new Express();
+
+export default class ExpressServer {
+  constructor() {
+    const root = path.normalize(`${__dirname}/../..`);
+    app.set('appPath', `${root}client`);
+    app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
+    app.use(
+      bodyParser.urlencoded({
+        extended: true,
+        limit: process.env.REQUEST_LIMIT || '100kb',
+      })
+    );
+    app.use(cookieParser(process.env.SESSION_SECRET));
+    app.use(Express.static(`${root}/public`));
+
+    const apiSpecPath = path.join(__dirname, 'api.yml');
+    app.use(process.env.OPENAPI_SPEC || '/spec', Express.static(apiSpecPath));
+    new OpenApiValidator({
+      apiSpecPath,
+    }).install(app);
+  }
+
+  router(routes) {
+    routes(app);
+    app.use(errorHandler);
+
+    return this;
+  }
+
+  listen(port = process.env.PORT) {
+    const welcome = p => () =>
+      l.info(
+        `up and running in ${process.env.NODE_ENV ||
+          'development'} @: ${os.hostname()} on port: ${p}}`
+      );
+    http.createServer(app).listen(port, welcome(port));
+    return app;
+  }
+}
