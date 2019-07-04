a5fbc22c5462271de3a593188eea0a0f93a44cfd
Rasmus Jepsen <r.jepsen@uqconnect.edu.au>
2019-07-04 10:33:16 +0800

    Added user middleware

--- backend/server/routes.ts
+++ backend/server/routes.ts
@@ -3,8 +3,9 @@ import examplesRouter from './api/controllers/examples/router';
 import authRouter from './api/controllers/auth/router';
 import groupRouter from './api/controllers/group/router';
 import authenticate from './api/middlewares/authenticate';
+import user from './api/middlewares/user';
 export default function routes(app: Application): void {
   app.use('/api/v1/examples', examplesRouter);
   app.use('/api/v1/auth', authenticate, authRouter);
-  app.use('/api/v1/group', authenticate, groupRouter);
+  app.use('/api/v1/group', authenticate, user, groupRouter);
 };
