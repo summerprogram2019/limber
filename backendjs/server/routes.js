c662f1d0f370ef60239fcad27c2e42b6a07b2e5d
Rasmus Jepsen <r.jepsen@uqconnect.edu.au>
2019-07-05 08:35:49 +0800

    Removed examples

--- backendjs/server/routes.js
+++ backendjs/server/routes.js
@@ -1,8 +1,6 @@
-import examplesRouter from './api/controllers/examples/router';
 import authRouter from './api/controllers/auth/router';
 import authenticate from './api/middlewares/authenticate';
 
 export default function routes(app) {
-  app.use('/api/v1/examples', examplesRouter);
   app.use('/api/v1/auth', authenticate, authRouter);
 }
