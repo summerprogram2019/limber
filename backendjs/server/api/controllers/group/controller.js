5f9920dfc080fc1da633763ea21f85498f225c80
Rasmus Jepsen <r.jepsen@uqconnect.edu.au>
2019-07-05 09:20:00 +0800

    Improved group controller Added api docs

--- backendjs/server/api/controllers/group/controller.js
+++ backendjs/server/api/controllers/group/controller.js
@@ -5,12 +5,22 @@ const Group = Model.Group;
 export class Controller {
   group(req, res) {
     const { name, description, tags } = req.body;
+    if (typeof req.body.id === "undefined") {
       let group = Group.create({
         name,
         description,
         tags,
         owner: req.user.sub
       });
+    }
+    else {
+      // TODO check privileges of user
+      const id = req.body.id;
+      let group = Group.findById(id);
+      group.name = name;
+      group.description = description;
+      group.tags = tags;
+    }
   }
 }
 export default new Controller();
