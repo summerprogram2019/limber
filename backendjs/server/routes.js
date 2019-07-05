b0510957c808fa5b34e3bcfb963277d93f0bc567
Rasmus Jepsen <r.jepsen@uqconnect.edu.au>
2019-07-05 10:29:12 +0800

    Fixed group routing

--- backendjs/server/routes.js
+++ backendjs/server/routes.js
@@ -1,6 +1,8 @@
 import authRouter from './api/controllers/auth/router';
+import groupRouter from './api/controllers/group/router';
 import authenticate from './api/middlewares/authenticate';
 
 export default function routes(app) {
   app.use('/api/v1/auth', authenticate, authRouter);
+  app.use('/api/v1/group', authenticate, groupRouter);
 }
