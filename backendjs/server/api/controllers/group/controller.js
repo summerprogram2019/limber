81a6177ff27d083a91f17eaaca5c6f7c1147d6c1
Rasmus Jepsen <r.jepsen@uqconnect.edu.au>
2019-07-05 09:46:03 +0800

    Fixed group controller

--- backendjs/server/api/controllers/group/controller.js
+++ backendjs/server/api/controllers/group/controller.js
@@ -5,22 +5,12 @@ const Group = Model.Group;
 export class Controller {
   group(req, res) {
     const { name, description, tags } = req.body;
-    if (typeof req.body.id === "undefined") {
-      let group = Group.create({
-        name,
-        description,
-        tags,
-        owner: req.user.sub
-      });
-    }
-    else {
-      // TODO check privileges of user
-      const id = req.body.id;
-      let group = Group.findById(id);
-      group.name = name;
-      group.description = description;
-      group.tags = tags;
-    }
+    let group = Group.create({
+      name,
+      description,
+      tags,
+      owner: req.user.sub
+    });
   }
 }
 export default new Controller();
