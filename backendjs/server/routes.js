42e9a5ba9ed80a5cbd8326783a87aea06046befa
Rasmus Jepsen <r.jepsen@uqconnect.edu.au>
2019-07-08 09:00:32 +0800

    Added user middleware to router

--- backendjs/server/routes.js
+++ backendjs/server/routes.js
@@ -4,5 +4,5 @@ import authenticate from './api/middlewares/authenticate';
 
 export default function routes(app) {
   app.use('/api/v1/auth', authenticate, authRouter);
-  app.use('/api/v1/group', authenticate, groupRouter);
+  app.use('/api/v1/group', authenticate, user, groupRouter);
 }
