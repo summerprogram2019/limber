542ca4f5b608a93e6a2eef5964e892afba04acb6
Rasmus Jepsen <r.jepsen@uqconnect.edu.au>
2019-07-08 09:30:16 +0800

    Fixed routes with user middleware

--- backendjs/server/routes.js
+++ backendjs/server/routes.js
@@ -1,6 +1,7 @@
 import authRouter from './api/controllers/auth/router';
 import groupRouter from './api/controllers/group/router';
 import authenticate from './api/middlewares/authenticate';
+import user from './api/middlewares/user';
 
 export default function routes(app) {
   app.use('/api/v1/auth', authenticate, authRouter);
