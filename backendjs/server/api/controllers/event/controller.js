ae1d8c8146b687a445144935f5b427e294cebc02
lrxqu <rxqu@hotmail.com>
2019-07-09 11:06:09 +0800

    add event apis

--- backendjs/server/api/controllers/event/controller.js
+++ backendjs/server/api/controllers/event/controller.js
@@ -1,28 +1,27 @@
 const Model = require('../../../sequelize/models');
 const Event = Model.Event;
+
 export class Controller {
-  async event(req, res) {
+  async getOne(req, res) {
     try {
-      const { name, description, tags } = req.body;
-      let event = await Event.create({
-        name,
-        description,
-        tags,
-        owner: req.user.sub
-      });
-      res.sendStatus(200).json({
-        success: true,
-        message: 'Successful',
-        data: {
-          name: event.name,
-          description: event.description,
-          next: event.next,
-          datetime: event.datetime,
-          length: event.length,
-          tags: event.tags,
-          owner: event.owner
+      const { id } = req.params;
+      let event = await Event.findOne({
+        where: {
+          id: id
         }
       });
+      if (event === null) {
+        res.status(400).json({
+          success: false,
+          message: 'Group does not exist'
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: 'Successful',
+          data: event
+        });
+      }
     } catch (error) {
       res.status(500).json({
         success: false,
@@ -30,6 +29,7 @@ export class Controller {
       });
     }
   }
+
   async get(req, res) {
     try {
       let events = await Event.findAll();
@@ -42,6 +42,74 @@ export class Controller {
       res.status(500).json({
         success: false,
         messasge: error.message
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      const { name, description, next, datetime, length, tags } = req.body;
+      let event = await Event.findOne({
+        where: {
+          id: id
+        }
+      });
+      if (event === null) {
+        res.status(400).json({
+          success: false,
+          message: 'Group does not exist'
+        });
+      }
+      if (event.owner !== req.user.sub) {
+        res.status(403).json({
+          success: false,
+          message: 'You are not the owner of the group'
+        });
+      }
+      let updated = await event.update({
+        name,
+        description,
+        next,
+        datetime,
+        length,
+        tags
+      });
+      res.status(200).json({
+        success: true,
+        message: 'Successful',
+        data: updated
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        messasge: error.message
+      });
+    }
+  }
+
+  async create(req, res) {
+    const { name, description, next, datetime, length, tags } = req.body;
+    const owner = req.user.sub;
+    try {
+      let event = await Event.create({
+        name,
+        description,
+        next,
+        datetime,
+        length,
+        tags,
+        owner
+      });
+      res.status(200).json({
+        success: true,
+        message: 'Successfully Created',
+        data: event
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message
       });
     }
   }
